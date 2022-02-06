d04fc08f78f38cc9d3b197fbdcc69f6276786661
NiverDaniel <niverdanielrm@ufps.edu.co>
2022-02-06 14:49:28 -0500

    fin seccion 4 - Router

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  exports: [],
+  imports: [CommonModule],
+})
+export class AuthModule {}
